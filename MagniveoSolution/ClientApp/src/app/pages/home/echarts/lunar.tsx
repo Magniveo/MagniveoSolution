d085bcecfb192079fe6ac24bd8447f5f8c9b6ecb
Magniveo <michailanatger@mail.ru>
2020-04-15 18:43:58 +0500

    Translate gradient

--- MagniveoSolution/ClientApp/src/app/pages/home/echarts/lunar.tsx
+++ MagniveoSolution/ClientApp/src/app/pages/home/echarts/lunar.tsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import ReactEcharts from 'echarts-for-react'
 import echarts from 'echarts';
-var dataAxis = ['点', '击', '柱', '子', '或', '者', '两', '指', '在', '触', '屏', '上', '滑', '动', '能', '够', '自', '动', '缩', '放'];
+var dataAxis = ['Точка', 'удар', 'столб', 'сын', 'или', 'человек', 'два', 'палец', 'на', 'касание', 'экран', 'вверх', 'слайд',' Переместить ',' Энергия ',' Достаточно ',' Я ',' Переместить ',' Сжать ',' Освободить '];
 var data = [220, 182, 191, 234, 290, 330, 310, 123, 442, 321, 90, 149, 210, 122, 133, 334, 198, 123, 125, 220];
 var yMax = 500;
 var dataShadow = [];
@@ -16,7 +16,7 @@ export default class Lunar extends PureComponent {
         }
         return {
             title: {
-                text: '特性示例：渐变色 阴影 点击缩放',
+                text: 'Пример функции: увеличение градиента тени тени',
                 subtext: 'Feature Sample: Gradient Color, Shadow, Click Zoom'
             },
             xAxis: {
